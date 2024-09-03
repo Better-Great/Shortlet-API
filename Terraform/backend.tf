e82c5eec437f440e95e86ad5e81f09ba233facfe
Better-Great <abiegbeg@gmail.com>
2024-09-03 16:30:30 +0100

    testing pipeline

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "ultra-task-434221-r5-terraform-state"
     prefix      = "terraform/state"
-    credentials = "/home/better-great/Downloads/keys.json"
+    credentials = "env:GOOGLE_CREDENTIALS"
   }
 }
---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+project_id       = "ultra-task-4r534221-"
+credentials_file = "env:GOOGLE_CREDENTIALS"
+region = "us-central1"
+alert_email = "abiegbeg@gmail.com"
